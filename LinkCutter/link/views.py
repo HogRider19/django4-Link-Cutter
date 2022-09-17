be1dcb8b7680b326c9d9f35865ec199d8f757aba
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-09-17 23:48:27 +0300

    Added create method!

--- LinkCutter/link/views.py
+++ LinkCutter/link/views.py
@@ -12,6 +12,14 @@ class LinkAPIViewSet(ModelViewSet):
     def get_queryset(self):
         return Link.objects.filter(user_id=self.request.user.id)
 
+    def create(self, request):
+        Link.objects.create(
+            name=request.name,
+            raw_url=request.raw_url,
+            truncated_url=request.truncated_url,
+            user=request.user,
+        )
+
 class RedirectView(View):
     def get(self, request, user_id, path):
         link = get_object_or_404(Link, truncated_url=path, user_id=user_id)
