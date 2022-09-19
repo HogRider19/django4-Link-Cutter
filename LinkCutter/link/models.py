59796e5420ad2e7f1dc38cbc84fc571badd531f1
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-09-19 14:16:51 +0300

    generate_success_url!

--- LinkCutter/link/models.py
+++ LinkCutter/link/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import Settings
 from django.contrib.auth.models import User
 
 
@@ -8,11 +9,19 @@ class Link(models.Model):
     name = models.CharField(max_length=300, blank=True, null=True)
     raw_url = models.URLField(max_length=5000)
     truncated_url = models.CharField(max_length=5000)
+    success_url = models.CharField(max_length=5000, blank=True, null=True)
     transition = models.PositiveBigIntegerField(default=0, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def save(self, *args, **kwargs) -> None:
+        self._generate_success_url()
+        return super().save(*args, **kwargs)
+
     def is_transition(self):
         self.transition += 1
+
+    def _generate_success_url(self):
+        self.success_url = f"{Settings.SITE_URL}/{self.truncated_url}"
 
     def __str__(self):
         return f"{self.name}"
--- LinkCutter/link/serializers.py
+++ LinkCutter/link/serializers.py
@@ -5,6 +5,7 @@ from .models import Link
 class LinkSerializer(serializers.ModelSerializer):
     """Сериализатор модели Link"""
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    success_url = serializers.ReadOnlyField()
 
     class Meta:
         model = Link
