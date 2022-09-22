5cb787634c5b78a667b14831b401476ed83fb4dd
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-09-22 15:20:57 +0300

    Small fix!

--- LinkCutter/app/urls.py
+++ LinkCutter/app/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-#from .yasg import urlpatterns as doc_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('link.urls')),
 ]
-
-
-#urlpatterns += doc_urls
